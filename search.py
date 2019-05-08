5351c5472139becf7b92c4a56d12de14ae12bcb1
Dawood Ikhlaq <daudmalik06@gmail.com>
2019-05-08 11:18:57 +0200

    accepts entity type, adds support for enabled list
    1:now it accepts the entity type e.g domain, url, ip
    2: add support for enabled file list means it will consider only list that are present in enabledLists.json, to see the format please check enabledLists.json.example file

--- search.py
+++ search.py
@@ -4,13 +4,38 @@ import json
 import os
 from glob import glob
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
   print("arguments are missing")
-  print "search.py pathToWarningLists stringToSearch"
+  print "search.py pathToWarningLists stringToSearch IOC_TYPE"
+  print "search.py ../misp-warninglists/lists/ google.com domain"
   sys.exit()
 
-stringToSearch = sys.argv[2]
+
 listPath = os.path.abspath(sys.argv[1])
+stringToSearch = sys.argv[2].strip()
+iocType = sys.argv[3]
+
+if iocType == 'url':
+    sys.exit()
+
+if iocType == 'domain' or iocType == 'hostname':
+    iocType = 'hostname'
+if iocType == 'ip':
+    iocType = 'cidr'
+
+
+enabledListsFile = os.path.abspath('./enabledLists.json')
+enabledListsFileExists = os.path.isfile(enabledListsFile)
+
+enabledLists = []
+if enabledListsFileExists:
+    enabledLists = open(enabledListsFile).read()
+    enabledLists = json.loads(enabledLists)
+
+allListsEnabled = False
+if len(enabledLists) == 0:
+    # no file present for enabled list means have to consider all the list
+    allListsEnabled = True
 
 lists = []
 warninglists = {}
@@ -20,8 +45,11 @@ for warninglist_file in glob(os.path.join(listPath, '*', 'list.json')):
 
 matches = []
 for warninglist in lists:
-    tmp = WarningList(warninglist, True)
-    if tmp.slowSearch(stringToSearch):
-        matches.append(warninglist['name'])
-
-print json.dumps(matches)
+    if allListsEnabled:
+        enabledLists.append(warninglist['name'])
+    if warninglist['name'] in enabledLists:
+        tmp = WarningList(warninglist, True)
+        if tmp.slowSearch(stringToSearch, iocType):
+            matches.append(warninglist['name'])
+if len(matches) != 0:
+    print json.dumps(matches)
