3df5ca12e03cb001a0b657f7cf9264ac32d1392b
Dawood Ikhlaq <daudmalik06@gmail.com>
2019-05-15 11:24:58 +0200

    Update search.py

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ import json
 import os
 from glob import glob
 import validators
+import tldextract
 
 if len(sys.argv) < 3:
   print("arguments are missing")
@@ -19,12 +20,18 @@ root_dir = os.path.dirname(os.path.realpath(__file__))
 iocType = ''
 if validators.domain(stringToSearch):
     iocType = 'hostname'
+    domain = tldextract.extract(stringToSearch)
+    currentDomain = domain.domain + '.' + domain.suffix
+    if domain.subdomain == 'www':
+        # remove www. from hostname
+        stringToSearch = currentDomain
 
 if validators.ipv4(stringToSearch):
     iocType = 'cidr'
 
 enabledListsFile = os.path.abspath(root_dir + '/enabledLists.json')
 enabledListsFileExists = os.path.isfile(enabledListsFile)
+
 
 enabledLists = []
 if enabledListsFileExists:
@@ -42,13 +49,19 @@ for warninglist_file in glob(os.path.join(listPath, '*', 'list.json')):
     with open(warninglist_file, 'r') as f:
         lists.append(json.load(f))
 
+tmpSearchString = stringToSearch
 matches = []
 for warninglist in lists:
     if allListsEnabled:
         enabledLists.append(warninglist['name'])
     if warninglist['name'] in enabledLists:
+        if 'Alexa' in warninglist['name'] and iocType == 'hostname':
+            # only for alexa top list we'll check domain name only
+            stringToSearch = currentDomain
         tmp = WarningList(warninglist, True)
         if tmp.slowSearch(stringToSearch, iocType):
             matches.append(warninglist['name'])
+        # reset the string to search
+        stringToSearch = tmpSearchString
 if len(matches) != 0:
     print json.dumps(matches)
